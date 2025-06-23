d947d74c7ad5bc8d3465ebefe24481e39c951c47
gcrumpton <gcrumpton@spartaglobal.com>
2025-06-23 14:56:31 +0100

    Add _shopHome locator

--- Pages/HomePage.cs
+++ Pages/HomePage.cs
@@ -5,10 +5,12 @@ namespace testing_polymer_shop_with_playwright.Pages
     public class HomePage
     {
         private readonly IPage _page;
+        private readonly ILocator _shopHome;
 
         public HomePage(IPage page)
         {
             _page = page;
+            _shopHome = _page.GetByRole(AriaRole.Link, new() { Name = "SHOP home" });
         }
 
         public async Task GotoAsync()
