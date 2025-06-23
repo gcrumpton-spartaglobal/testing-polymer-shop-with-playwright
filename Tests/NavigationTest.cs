7aa2292699501bc816da90a8ffac997a50917bc6
gcrumpton <gcrumpton@spartaglobal.com>
2025-06-23 14:35:35 +0100

    Add CheckHomePageTitle()

--- Tests/NavigationTest.cs
+++ Tests/NavigationTest.cs
@@ -1,6 +1,23 @@
-﻿namespace testing_polymer_shop_with_playwright.Tests
+﻿using Microsoft.Playwright;
+using Xunit;
+using testing_polymer_shop_with_playwright.Pages;
+
+namespace testing_polymer_shop_with_playwright.Tests
 {
     public class NavigationTest
     {
+        [Fact]
+        public async Task CheckHomePageTitle()
+        {
+            // Arrange
+            using var playwright = await Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
+            var page = new HomePage(await browser.NewPageAsync());
+            // Act
+            await page.GotoAsync();
+            var title = await page.TitleAsync();
+            // Assert
+            Assert.Equal("Home - SHOP", title);
+        }
     }
 }
