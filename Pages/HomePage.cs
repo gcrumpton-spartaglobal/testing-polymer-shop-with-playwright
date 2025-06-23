c5903c5832f4ec68ba6c1460c6ce88ec7576dfa2
gcrumpton <gcrumpton@spartaglobal.com>
2025-06-23 14:16:20 +0100

    Add _page field and HomePage(IPage) constructor

--- Pages/HomePage.cs
+++ Pages/HomePage.cs
@@ -1,6 +1,14 @@
-﻿namespace testing_polymer_shop_with_playwright.Pages
+﻿using Microsoft.Playwright;
+
+namespace testing_polymer_shop_with_playwright.Pages
 {
     public class HomePage
     {
+        private readonly IPage _page;
+
+        public HomePage(IPage page)
+        {
+            _page = page;
+        }
     }
 }
